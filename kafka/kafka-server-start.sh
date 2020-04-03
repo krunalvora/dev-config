da223ed672ec8227664e4359db161b505b1949d7
Krunal Vora <krunalvora4@gmail.com>
2020-04-03 13:59:32 -0700

    adding kafka heap opts before starting kafka server

--- kafka/kafka-server-start.sh
+++ kafka/kafka-server-start.sh
@@ -4,6 +4,8 @@ MY_KAFKA_HOME="${KAFKA_HOME:-/usr/local/kafka}"
 SERVER_PROPERTIES="${1:-server.properties}"
 SERVER_PROP_PATH="$MY_KAFKA_HOME/config/$SERVER_PROPERTIES"
 
+export KAFKA_HEAP_OPTS="-Xmx1G -Xms1G"
+
 # Start the kafka server 
 $MY_KAFKA_HOME/bin/kafka-server-start.sh -daemon $SERVER_PROP_PATH
 
